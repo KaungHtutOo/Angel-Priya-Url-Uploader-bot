53772fd04707fa61f7e656c7ebb67176e52921b4
KaungHtutOo <73459374+KaungHtutOo@users.noreply.github.com>
2023-03-01 13:39:04 +0630

    Update translation.py

--- translation.py
+++ translation.py
@@ -9,15 +9,15 @@ Just send me urls. You can send HTTP/HTTPS direct links. I will check and start 
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
-    DOWNLOAD_START = "Download started in **Lazy mode...**"
+    DOWNLOAD_START = "Download started in **@NgaHtutmm mode...**"
     UPLOAD_START = "Processing upload..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thank you <a href='https://t.me/LazyDeveloperr'>**LazyDeveloperr**</a> for helping us in this journey ❤️"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@LazyDeveloperr"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thank you <a href='https://t.me/NgaHtutmm'>**Nga Htut**</a> for helping us in this journey ❤️"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@NgaHtutmm"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>LazyDeveloperr</b> said: {}"
+    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>Nga Htut</b> said: {}"
     HELP_USER = """How to Use Me? Follow These steps!
     
 1. Send url (example.domain/File.mp4 | New Filename.mp4).
@@ -28,7 +28,7 @@ URL | filename | username | password"""
    Video  - Give File as video without Screenshots
    File   - Give File without Screenshots
 
-Contact ADMIN if any error occurs @LazyDeveloperr"""
+Contact ADMIN if any error occurs @NgaHtutmm"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /genthumbnail to a media album, to generate custom thumbnail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
     CANCEL_STR = "Process Cancelled"
